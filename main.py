99baeb7b46204ef31da6027bf17e672718759fff
tysweezy <tsouza.sf@gmail.com>
2013-03-22 11:03:51 -0700

    added python file

---
+++ main.py
@@ -0,0 +1,21 @@
+#Just some fun with python
+#war-games
+#main.py
+#Author: Tyler Souza
+
+def main():
+    name = raw_input("Enter you name: ")
+    print "Welcome", name
+    play_game = raw_input("Do you want to play a game? ")
+    
+    #checks if user say yes your no
+    if play_game == "yes" and "Yes" and "YES":
+        print "How about a nice game of chess?"
+    else:
+        print "Ok, would you like to play a game?"
+        
+    
+
+   
+
+main()
